80fd5747f222ef3a59852366fef2974248a8caf6
Artem Yakovskij <62523761+t-ogni@users.noreply.github.com>
2024-05-06 14:33:11 +0300

    inital

---
+++ Lab16-test/BankAccountTest.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using BankAccountNS;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BankTest
+{
+    [TestClass]
+    public class BankAccountTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        // unit test code
+        [TestMethod]
+        public void Debit_WithValidAmount_UpdatesBalance()
+        {
+            // arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 4.55;
+            double expected = 7.44;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            // act
+            account.Debit(debitAmount);
+
+            // assert
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+        }
+
+        //unit test method
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Debit_WhenAmountIsLessThanZero_ShouldThrowArgumentOutOfRange()
+        {
+            // arrange
+            double beginningBalance = 11.99;
+            double debitAmount = -100.00;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            // act
+            account.Debit(debitAmount);
+
+            // assert is handled by ExpectedException
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Debit_WhenAmountIsMoreThanBalance_ShouldThrowArgumentOutOfRange()
+        {
+            // arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 8000.00;
+            BankAccount account = new BankAccount("Mr. Byran Walton", beginningBalance);
+
+            // acting
+            account.Debit(debitAmount);
+
+            // assert is handled by ExpectedException
+        }
+
+        //unit test method
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Debit_WhenBalanceIsLessThanZero_ShouldThrowArgumentOutOfRange()
+        {
+            // arrange
+            double beginningBalance = -100.0;
+            double debitAmount = 10.00;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            // act
+            account.Debit(debitAmount);
+
+            // assert is handled by ExpectedException
+        }
+        //unit test method
+        [TestMethod]
+        public void Debit_WhenBalanceIsBigNumber_ShouldThrowArgumentOutOfRange()
+        {
+            // arrange
+            double beginningBalance = 10e50;
+            double debitAmount = 10.00;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            // act
+            account.Debit(debitAmount);
+
+            double actual = account.Balance;
+            Assert.AreEqual(10e50-10, actual, 0.001, "Account not debited correctly");
+
+        }
+
+        //unit test method
+        [TestMethod]
+        public void Debit_WhenZeros_ShouldThrowArgumentOutOfRange()
+        {
+            // arrange
+            double beginningBalance = 0.0;
+            double debitAmount = 0.0;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            // act
+            account.Debit(debitAmount);
+
+            double actual = account.Balance;
+            Assert.AreEqual(0, actual, 0.001, "Account not debited correctly");
+        }
+    }
+
+}
---
+++ Lab16-test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Lab16-test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lab16-test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1d4d05d1-adb2-44ab-922e-b8d8b7618fad")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
